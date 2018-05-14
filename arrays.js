52f39a26fb8ec060e0de5f7edf344226b4b3dede
impactlearner <time.darden@gmail.com>
2018-05-14 22:50:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array, element){
-  var new_array = [element, array[0]];
+  var new_array = [element, array[0]]
   return new_array
 }
 
@@ -10,21 +10,21 @@ function destructivelyAddElementToBeginningOfArray (array, element){
   return array
 }
 function addElementToEndofArray(array, element){
-  array.push(element);
+  array.push(element)
   return array
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
+  array.push(element)
   return array
 }
 function accessElementInArray(array, index){
   return array[2]
 }
 function removeElementFromBeginningOfArray(array){
-  array.shift();
+  array.shift()
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.pop();
+  array.pop()
   return array
 }
