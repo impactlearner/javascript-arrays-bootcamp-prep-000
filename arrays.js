637a23ad305fb9d3c7da6f3efce44232eb6fc2b8
impactlearner <time.darden@gmail.com>
2018-05-14 22:57:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (array, element){
-  return = [element, ...array]
+  var new=[element, array[0]]
+  return new
 }
