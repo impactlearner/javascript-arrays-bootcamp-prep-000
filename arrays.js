a63d47b2cdf786cc742d0e0f09e8a32b5a60d4f8
impactlearner <time.darden@gmail.com>
2018-05-14 23:00:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,19 @@ function addElementToBeginningOfArray (array, element){
   var new_array=[element, array[0]]
   return new_array
 }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
