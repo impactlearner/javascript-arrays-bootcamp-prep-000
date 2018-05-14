5832900b07696c11cd93fcbc0e1ea55d123496ce
impactlearner <time.darden@gmail.com>
2018-05-14 22:35:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var addElementToBeginningOfArray = ['sun', 'moon', 'stars'];
+['comet'], ...addElementToBeginningOfArray]
+
+
+var destructivelyAddElementToBeginningOfArray = ['dog', 'cat', 'pig'];
+destructivelyAddElementToBeginningOfArray.unshift('rat')
+
