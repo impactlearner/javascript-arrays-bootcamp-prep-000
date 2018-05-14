4a21dea0b7338648b6f8a457c24f9fb3fc7694a6
impactlearner <time.darden@gmail.com>
2018-05-14 23:47:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 function addElementToEndOfArray(array, element){
-  array.push('element')
+  array.push(element)
   return array
 }
 function destructivelyAddElementToEndOfArray(array, element){
