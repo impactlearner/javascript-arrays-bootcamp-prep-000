0c3ab0a882410ee228268b3069f4448927a00067
impactlearner <time.darden@gmail.com>
2018-05-15 00:27:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,29 +1,29 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (array, element){
-  var new_array=[element, array[0]]
-  return new_array
+  var new_array=[element, array[0]];
+  return new_array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 function addElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 function accessElementInArray(array, index){
-  return array[index]
+  return array[index];
 }
 function removeElementFromBeginningOfArray(array){
-  array.shift()
-  return array
+  array.shift();
+  return array;
 }
 function removeElementFromEndOfArray(array){
-  array.pop()
-  return array
+  array.pop();
+  return array;
 }
